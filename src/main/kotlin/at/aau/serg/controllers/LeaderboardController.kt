179d198ccb9f0aa88d658d0bf51fc75fb990e0ba
Stefan Ckonjovic <stefanck@edu.aau.at>
2025-03-11 21:27:32 +0100

    initial commit

---
+++ src/main/kotlin/at/aau/serg/controllers/LeaderboardController.kt
@@ -0,0 +1,21 @@
+package at.aau.serg.controllers
+
+import at.aau.serg.models.GameResult
+import at.aau.serg.services.GameResultService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/leaderboard")
+class LeaderboardController(
+    private val gameResultService: GameResultService
+) {
+
+    @GetMapping
+    fun getLeaderboard(): List<GameResult> =
+        gameResultService.getGameResults().sortedWith(compareByDescending<GameResult> { it.score }
+            .thenBy { it.id })
+
+}
+
